1c9c39249a0ad2fc0f64775efeb5bf263c83487d
Developerzakir <zakir1234@!za>
2026-01-19 12:52:42 +0600

    input box data display with useState object

---
+++ my-react-app/src/components/UseStateObject.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const UseStateObject = () => {
+    const [info,setInfo] = useState({
+        name:"",
+        id:null
+    })
+
+  return (
+    <div>
+        <form>
+                <input value={info.name} onChange={(e)=>setInfo({...info, name:e.target.value})} />
+                <input value={info.id} onChange={(e)=>setInfo({...info, id:e.target.value})} />
+        </form>
+
+        <div>
+            <p>Name: {info.name}</p>
+            <p>Id: {info.id}</p>
+        </div>
+    </div>
+  )
+}
+
+export default UseStateObject
